27154bd31e189f595bfce5556877e591043a1bd9
JK <junq.jeon@gmail.com>
2021-03-22 11:52:17 +0900

    aaa

---
+++ AiLevelTestKit/Classes/Common/ColourKit.swift
@@ -0,0 +1,59 @@
+//
+//  ColourKit.swift
+//  Baobab
+//
+//  Created by Jun-kyu Jeon on 15/03/2020.
+//  Copyright © 2020 Cocoslab. All rights reserved.
+//
+
+import UIKit
+
+public struct ColourKit {
+    public struct Code {
+        public static let Hex000000 = UIColor(named: "000000") ?? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        public static let Hex121212 = UIColor(named: "121212") ?? #colorLiteral(red: 0.07058823529, green: 0.07058823529, blue: 0.07058823529, alpha: 1)
+        public static let Hex222222 = UIColor(named: "222222") ?? #colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1)
+        public static let Hex27272C = UIColor(named: "27272C") ?? #colorLiteral(red: 0.1529411765, green: 0.1529411765, blue: 0.1725490196, alpha: 1)
+        public static let Hex3E3A39 = UIColor(named: "3E3A39") ?? #colorLiteral(red: 0.2431372549, green: 0.2274509804, blue: 0.2235294118, alpha: 1)
+        public static let Hex555555 = UIColor(named: "555555") ?? #colorLiteral(red: 0.3333333333, green: 0.3333333333, blue: 0.3333333333, alpha: 1)
+        public static let Hex888888 = UIColor(named: "888888") ?? #colorLiteral(red: 0.5333333333, green: 0.5333333333, blue: 0.5333333333, alpha: 1)
+        public static let Hex8A8A8A = UIColor(named: "8A8A8A") ?? #colorLiteral(red: 0.5411764706, green: 0.5411764706, blue: 0.5411764706, alpha: 1)
+        public static let HexAAAAAA = UIColor(named: "AAAAAA") ?? #colorLiteral(red: 0.6666666667, green: 0.6666666667, blue: 0.6666666667, alpha: 1)
+        public static let HexA0A0A0 = UIColor(named: "A0A0A0") ?? #colorLiteral(red: 0.6274509804, green: 0.6274509804, blue: 0.6274509804, alpha: 1)
+        public static let HexB3B3B3 = UIColor(named: "B3B3B3") ?? #colorLiteral(red: 0.7019607843, green: 0.7019607843, blue: 0.7019607843, alpha: 1)
+        public static let HexC0C0C0 = UIColor(named: "C0C0C0") ?? #colorLiteral(red: 0.7529411765, green: 0.7529411765, blue: 0.7529411765, alpha: 1)
+        public static let HexCCCCCC = UIColor(named: "CCCCCC") ?? #colorLiteral(red: 0.8, green: 0.8, blue: 0.8, alpha: 1)
+        public static let HexCDDBE0 = UIColor(named: "CDDBE0") ?? #colorLiteral(red: 0.8039215686, green: 0.8588235294, blue: 0.8784313725, alpha: 1)
+        public static let HexD0DBDF = UIColor(named: "D0DBDF") ?? #colorLiteral(red: 0.8156862745, green: 0.8588235294, blue: 0.8745098039, alpha: 1)
+        public static let HexD0D0D0 = UIColor(named: "D0D0D0") ?? #colorLiteral(red: 0.8156862745, green: 0.8156862745, blue: 0.8156862745, alpha: 1)
+        public static let HexEEEEEE = UIColor(named: "EEEEEE") ?? #colorLiteral(red: 0.9333333333, green: 0.9333333333, blue: 0.9333333333, alpha: 1)
+        public static let HexF0F0F0 = UIColor(named: "F0F0F0") ?? #colorLiteral(red: 0.9411764706, green: 0.9411764706, blue: 0.9411764706, alpha: 1)
+        public static let HexFFFFFF = UIColor(named: "FFFFFF") ?? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        
+        public static let Hex727B88 = UIColor(named: "727b88") ?? #colorLiteral(red: 0.4470588235, green: 0.4823529412, blue: 0.5333333333, alpha: 1)
+        
+        public struct Static {
+            
+            public static let Hex00C9B7 = UIColor(named: "00c9b7_static") ?? #colorLiteral(red: 0, green: 0.7882352941, blue: 0.7176470588, alpha: 1)
+            public static let Hex222222 = UIColor(named: "222222_static") ?? #colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1)
+            public static let Hex3296D7 = UIColor(named: "3296d7_static") ?? #colorLiteral(red: 0.1960784314, green: 0.5882352941, blue: 0.8431372549, alpha: 1)
+            public static let Hex2D78CD = UIColor(named: "2d78cd_static") ?? #colorLiteral(red: 0.1764705882, green: 0.4705882353, blue: 0.8039215686, alpha: 1)
+            public static let HexF764AB = UIColor(named: "f764ab_static") ?? #colorLiteral(red: 0.968627451, green: 0.3921568627, blue: 0.6705882353, alpha: 1)
+            public static let HexE1B496 = UIColor(named: "e1b496_static") ?? #colorLiteral(red: 0.8823529412, green: 0.7058823529, blue: 0.5882352941, alpha: 1)
+            public static let Hex69A3ED = UIColor(named: "69a3ed_static") ?? #colorLiteral(red: 0.4117647059, green: 0.6392156863, blue: 0.9294117647, alpha: 1)
+            public static let Hex5CC4BA = UIColor(named: "5cc4ba_static") ?? #colorLiteral(red: 0.3607843137, green: 0.768627451, blue: 0.7294117647, alpha: 1)
+            public static let Hex5DD3BC = UIColor(named: "5dd3bc_static") ?? #colorLiteral(red: 0.3647058824, green: 0.8274509804, blue: 0.737254902, alpha: 1)
+            public static let Hex7F58D2 = UIColor(named: "7f58d2_static") ?? #colorLiteral(red: 0.4980392157, green: 0.3450980392, blue: 0.8235294118, alpha: 1)
+            public static let Hex2898C5 = UIColor(named: "2898c5_static") ?? #colorLiteral(red: 0.1568627451, green: 0.5960784314, blue: 0.7725490196, alpha: 1)
+            public static let HexA9D69C = UIColor(named: "a9d69c_static") ?? #colorLiteral(red: 0.662745098, green: 0.8392156863, blue: 0.6117647059, alpha: 1)
+            public static let HexC343A7 = UIColor(named: "c343a7_static") ?? #colorLiteral(red: 0.7647058824, green: 0.262745098, blue: 0.6549019608, alpha: 1)
+            public static let HexF44F69 = UIColor(named: "f44f69_static") ?? #colorLiteral(red: 0.9568627451, green: 0.3098039216, blue: 0.4117647059, alpha: 1)
+            public static let HexFB654F = UIColor(named: "fb654f_static") ?? #colorLiteral(red: 0.9843137255, green: 0.3960784314, blue: 0.3098039216, alpha: 1)
+            public static let HexFDBC58 = UIColor(named: "fdbc58_static") ?? #colorLiteral(red: 0.9921568627, green: 0.737254902, blue: 0.3450980392, alpha: 1)
+            public static let HexFA4175 = UIColor(named: "fa4175_static") ?? #colorLiteral(red: 0.9803921569, green: 0.2549019608, blue: 0.4588235294, alpha: 1)
+            public static let HexFFFF91 = UIColor(named: "ffff91_static") ?? #colorLiteral(red: 1, green: 1, blue: 0.568627451, alpha: 1)
+        }
+    }
+}
+
+
